f0b1b59dd90c653c1cbe167d06cfe39836491784
Daniel Briceno <daniel.b100726@gmail.com>
2025-01-22 19:22:54 -0400

    basic post

---
+++ src/posts/post.entity.ts
@@ -0,0 +1,11 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Post {
+  @Field((type) => Int)
+  id: number;
+  @Field()
+  title: string;
+  @Field({ nullable: true })
+  content?: string;
+}
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { Post } from './post.entity';
+
+@Injectable()
+export class PostsService {
+  findAll(): Post[] {
+    return [
+      { id: 1, title: 'Post 1', content: 'Content 1' },
+      { id: 2, title: 'Post 2', content: 'Content 2' },
+      { id: 3, title: 'Post 3', content: 'Content 3' },
+    ];
+  }
+}
